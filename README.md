b1634ef12e61d9d1214019255160acbf0c99adde
Your Name <you@example.com>
2024-08-06 07:23:00 -0500

    setup completed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Microfrontend in React
 This project deals with building a front end UI using microfrontend architecture
+## Proposed Architecture
+Below is the overall Architecture
+!["Architecture"](./images/1.PNG) 
---
+++ ecomm/container/webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    compress: true,
+    port: 8080,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
